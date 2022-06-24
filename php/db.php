f38cd0d15ee3290dae171d6713561764bf942c33
johnwaithira <waithirajon@gmail.com>
2022-06-25 01:02:02 +0300

    PHP Login system with Email Verification

--- php/db.php
+++ php/db.php
@@ -2,5 +2,30 @@
 
 class Database
 {
-    
+    public $dsn = "mysql:host=localhost; dbname=phploginsystem";
+    public $user = "root";
+    public $pass = "1234";
+
+    private $conn = null;
+    public function __construct()
+    {
+        try
+        {
+            $this->conn = new PDO($this->dsn, $this->user, $this->pass);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            echo "Connected";
+
+        }
+        catch(Exception $e)
+        {
+            echo "<pre>";
+            var_dump($e);
+            echo "</pre>";
+
+        }
+    }
+
 }
+
+$db = new Database();
