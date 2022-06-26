23ed260f5eed6463b1f47c981c9e9f9323e668d2
johnwaithira <waithirajon@gmail.com>
2022-06-26 14:22:21 +0300

    Passwift Development

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $app::session();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP login system</title>
     <link rel="stylesheet" href="http://127.0.0.1/shoetailor/ke/css/style.css">
 
 </head>
