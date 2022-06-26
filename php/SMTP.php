ae98699c45432f354c4bb34b8ab7cd4be79b49cf
johnwaithira <waithirajon@gmail.com>
2022-06-26 12:26:44 +0300

    Passwift Development

--- php/SMTP.php
+++ php/SMTP.php
@@ -2,6 +2,10 @@
 require_once "./PHPMailer/Exception.php";
 require_once "./PHPMailer/SMTP.php";
 require_once "./PHPMailer/PHPMailer.php";
+
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 class SMTP
 {
     public static function email($params = [])
