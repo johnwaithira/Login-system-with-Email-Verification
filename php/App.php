e72b62fd7e1e9940e1e6bf74cbf0fe1cf2fad4c3
johnwaithira <waithirajon@gmail.com>
2022-06-26 14:24:04 +0300

    Passwift Development

--- php/App.php
+++ php/App.php
@@ -15,7 +15,15 @@ class App
     public static function header()
     {
         echo '
-        
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="http://127.0.0.1/shoetailor/ke/css/style.css">
+        </head>
+        <body>
         ';
     }
 
