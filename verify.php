99440b2b43f1dc37bacc9d310a4dd35b65ff833c
johnwaithira <waithirajon@gmail.com>
2022-06-26 14:22:52 +0300

    Passwift Development

--- verify.php
+++ verify.php
@@ -1,1 +1,15 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verify Account</title>
+    <link rel="stylesheet" href="http://127.0.0.1/shoetailor/ke/css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
